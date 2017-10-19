c0c74f49dbd68d537a0f13d09a62eba527a80876
Никита Боков <nik-7777@yandex.ru>
2017-10-20 00:05:48 +0300

    1. Приложение переименовано в VK_NikitaBokov 2. В приложение добавлены: - UITabbarViewController - UITableViewController x 3 - UICollectionViewController 3. Реализована проверка логина/пароля и переход по нажатию кнопки на UITabbarViewController 4. Добавлены две вкладки в UITabbarViewController 5. На первой вкладке настроена история пользователя (User Story) UINavigationController -> UITableViewController - >  UICollectionViewController 6. На второй вкладке UINavigationController - >  UITableViewController - >  UITableViewController

--- VK/LoginFormController.swift
+++ VK/LoginFormController.swift
@@ -12,7 +12,8 @@ class LoginFormController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        LoginField.text = "admin"
+        PasswordField.text = "123456"
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +22,23 @@ class LoginFormController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBOutlet weak var LoginField: UITextField!
+    
+    @IBOutlet weak var PasswordField: UITextField!
+    
+    @IBAction func Enter(_ sender: UIButton) {
+        if LoginField.text == "admin" &&
+            PasswordField.text == "123456" {
+        performSegue(withIdentifier: "go1", sender: nil)
+        } else {
+            let alert = UIAlertController(title: "Ошибка", message: "Введены неверные данные", preferredStyle: .alert)
+            let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alert.addAction(action)
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
     /*
     // MARK: - Navigation
 
